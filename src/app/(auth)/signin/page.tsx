9daca9f0394f138408050c99748ab48985cf003b
Victor Casas <57921784+vicasas@users.noreply.github.com>
2025-05-11 21:20:29 -0400

    feat(SignInPage): Implement page structure and UI

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,7 +1,35 @@
-export default function SignInPage() {
+import NextLink from 'next/link'
+import { Box, Container, Link, Typography } from '@mui/material'
+import SignInForm from '@/components/SignInForm'
+
+export default async function SignInPage() {
   return (
-    <div>
-      <h1>Sign In</h1>
-    </div>
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{ display: 'flex', flexDirection: 'column', minHeight: '100dvh' }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '100%',
+          mt: 4,
+        }}
+      >
+        <Typography component="h1" variant="h4" sx={{ mb: 0.5 }}>
+          Sign in
+        </Typography>
+        <Typography color="text.secondary">
+          {"Don't have an account?"}{' '}
+          <Link href="/signup" component={NextLink}>
+            Sign Up
+          </Link>
+          .
+        </Typography>
+        <SignInForm />
+      </Box>
+    </Container>
   )
 }
